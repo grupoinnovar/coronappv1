fe9e52fb7378eb93444bfd3da7588b5e17b4732f
Grupo Innovar <grupoinnovar@iMac-de-Grupo.local>
2020-03-23 15:36:17 -0500

    inicio

---
+++ lib/common/constants.dart
@@ -0,0 +1,28 @@
+library constants;
+
+// Your wordpress website URL
+const String WORDPRESS_URL = "https://coronapp.co/blog";
+
+// Featured category ID (for Home Screen top section)
+const int FEATURED_ID = 11;
+
+// Tab 2 page category name
+const String PAGE2_CATEGORY_NAME = "Tips";
+
+// Tab 2 page category ID
+const int PAGE2_CATEGORY_ID = 11;
+
+// Custom categories in search tab
+// Array in format
+// ["Category Name", "Image Link", "Category ID"]
+const List<dynamic> CUSTOM_CATEGORIES = [
+  ["Lifestyle", "assets/boxed/lifestyle.png", 11],
+  ["Fashion", "assets/boxed/fashion.png", 11],
+  ["Music", "assets/boxed/music.png", 11],
+  ["Photography", "assets/boxed/photography.png", 11],
+  ["Sport", "assets/boxed/sport.png", 11],
+  ["World", "assets/boxed/world.png", 11],
+  ["Health", "assets/boxed/health.png", 11],
+  ["Travel", "assets/boxed/travel.png", 11],
+  ["Recipies", "assets/boxed/recipies.png", 11],
+];
